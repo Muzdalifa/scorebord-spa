f82d11a3f894b278c2bedd25a97b6cd8bd7c4e76
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-10-31 13:17:53 +0100

    create main layout components.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Main from './layout/main';
+import 'milligram/dist/milligram.css'
 
-ReactDOM.render( <h1> Welcome to Scoreboard</h1>, document.getElementById('root') );
+ReactDOM.render(<Main />, document.getElementById('root') )
 
---
+++ src/layout/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './main.css';
+
+const Main = () =>{
+  
+  return <div>
+    <div className='header'></div>
+
+    <div className='body'></div>
+
+    <div className='footer'></div>
+  </div>
+}
+
+export default Main;
