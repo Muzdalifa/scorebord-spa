30a8a2f7b6d8346e50651d5214126ef3bac2523c
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-10-31 19:27:28 +0100

    Add score board component

---
+++ src/components/score-table.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const ScoreTable = () =>{
+  return <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Score</th>
+        <th>Win</th>
+        <th>Loose</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Stephen Curry</td>
+        <td>27</td>
+        <td>1,91</td>
+        <td>Akron, OH</td>
+      </tr>
+      <tr>
+        <td>Klay Thompson</td>
+        <td>25</td>
+        <td>2,01</td>
+        <td>Los Angeles, CA</td>
+      </tr>
+    </tbody>
+ </table>
+}
+export default ScoreTable;
