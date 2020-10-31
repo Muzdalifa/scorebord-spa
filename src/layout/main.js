ec8912625806007b11c0fba2be5482953046f3e4
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-10-31 19:27:52 +0100

    Add player and score board to main.

--- src/layout/main.js
+++ src/layout/main.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import ScoreTable from '../components/score-table';
+import Header from '../components/header';
 import './main.css';
+import AddPlayer from '../components/add-player';
 
 const Main = () =>{
   
   return <div>
-    <div className='header'></div>
+    <div className='header'>
+      <Header />
+    </div>
 
-    <div className='body'></div>
+    <div className='body'>
+      <AddPlayer />
+      <ScoreTable />
+    </div>
 
-    <div className='footer'></div>
+    <div className='footer'>
+
+    </div>
   </div>
 }
 
