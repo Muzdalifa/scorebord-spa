d518cb551c7361c8a125a7c2ee408634f0b61c42
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-10-31 19:26:30 +0100

    Add header

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () =>{
+  return <div>
+    <div>
+      <h1>Welcome to Scoreboard</h1>
+    </div>
+    <div>
+      
+    </div>
+  </div>
+}
+
+export default Header;
