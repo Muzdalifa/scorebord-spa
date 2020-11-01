efdb6aeb11ac2a13cdb8ab0bbf6244a9eccc89f4
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-11-01 11:45:47 +0100

    Add player using add button.

--- src/components/score-table.js
+++ src/components/score-table.js
@@ -1,44 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './score-table.css';
 
+const row = (record, index) =>{
+  return <tr key={index}>
+    <td>{record.name}</td>
+    <td>{record.win}</td>
+    <td>{record.loose}</td>
+    <td>{index + 1}</td>
+  </tr>
+}
+
+const compareRecords = ( recordA, recordB) =>{
+
+  const scoreA = recordA.win - recordA.loose
+  const scoreB = recordB.win - recordB.loose
+
+  if(scoreA > scoreB)
+    return 1;
+  else if(scoreA < scoreB)
+    return -1;
+  else return 0;
+}
+
+/**
+ * 
+ * @param {{name: string, win:number, loose:number}[]} records 
+ */
+const addPlayer = (records)=>{
+
+  const newPlayer = {
+    name: document.getElementById('playerName').value,
+    win: 0,
+    loose: 0
+  }
+
+  records.push(newPlayer);
+  return [...records]; //spread array: create new array object with new records
+  
+}
 
 const ScoreTable = () =>{
 
-
-  const records = [
-    {name: 'Makame', win:1, loose:2},
-    {name: 'Muzda', win:2, loose:3},
-    {name: 'Mahmud', win:1, loose:0},
-    {name: 'Selma', win:2, loose:2},
-    {name: 'Hamza', win:1, loose:1}
-  ];
-
-  const row = (record, index) =>{
-    return <tr key={index}>
-      <td>{record.name}</td>
-      <td>{record.win}</td>
-      <td>{record.loose}</td>
-      <td></td>
-    </tr>
-  }
-
-
-  const compareRecords = ( recordA, recordB) =>{
-
-    const scoreA = recordA.win - recordA.loose
-    const scoreB = recordB.win - recordB.loose
-
-    if(scoreA > scoreB)
-      return 1;
-    else if(scoreA < scoreB)
-      return -1;
-    else return 0;
-  }
+  const [records, setRecords] = useState([]);
 
   return <div>
     <div>
-      <input  type='text' placeholder='Enter Name'/>
-      <button className='add-button'>Add</button>
+      <input  type='text' id='playerName' placeholder='Enter Name'/>
+      <button className='add-button' onClick={ ()=> setRecords( addPlayer(records) ) }>Add</button>
     </div>
 
     <table>
